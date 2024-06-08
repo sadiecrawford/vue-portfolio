5fafda97569324ff6bc340c2c62376704fd216f7
Zach Young <turniptrader@gmail.com>
2024-06-08 12:49:13 -0700

    support animations

--- src/components/portfolio_elements/PortfolioPreview.vue
+++ src/components/portfolio_elements/PortfolioPreview.vue
@@ -17,6 +17,10 @@ export default {
 </script>
 
 <style scoped>
+li {
+  animation: var(--global-animation-fade-up);
+}
+
 .static-image {
   max-width: 100%;
   height: auto; /* Ensures the image scales while maintaining its aspect ratio */
