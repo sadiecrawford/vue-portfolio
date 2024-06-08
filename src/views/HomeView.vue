b1a8820e9c1e14a89e015fe9b4fae0f0b8200c04
Zach Young <turniptrader@gmail.com>
2024-06-08 12:50:06 -0700

    fix import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import VerticalBox from '../components/VerticalBox.vue'
+import HorizontalBox from '../components/HorizontalBox.vue'
 import SimpleLink from '../components/SimpleLink.vue'
 import PortfolioSection from '../components/portfolio_elements/PortfolioSection.vue'
 import PortfolioPreview from '../components/portfolio_elements/PortfolioPreview.vue'
@@ -15,23 +16,23 @@ import img_DentaLabPreview from '@/assets/portfolio/games/dentalab/DentaLabPrevi
     <VerticalBox>
       <h1 class="center-wrapper">Work.</h1>
       <HorizontalBox class="center-wrapper">
-        <SimpleLink target="Work">Games</SimpleLink>
-        <SimpleLink target="VR Research">VR Research</SimpleLink>
-        <SimpleLink target="Software">Software</SimpleLink>
+        <SimpleLink href="#work">Games</SimpleLink>
+        <SimpleLink href="#vr">VR Research</SimpleLink>
+        <SimpleLink href="#software">Software</SimpleLink>
       </HorizontalBox>
 
-      <h2 class="left-wrapper">Games.</h2>
+      <h2 class="left-wrapper" id="games">Games.</h2>
       <PortfolioSection>
         <PortfolioPreview :imageSrc="img_UnmaskedPreview" />
         <PortfolioPreview :imageSrc="img_HushPreview" />
       </PortfolioSection>
 
-      <h2 class="left-wrapper">VR Research.</h2>
+      <h2 class="left-wrapper" id="vr">VR Research.</h2>
       <PortfolioSection>
         <PortfolioPreview :imageSrc="img_MouthPreview" />
         <PortfolioPreview :imageSrc="img_DentaLabPreview" />
       </PortfolioSection>
-      <h2 class="left-wrapper">Software.</h2>
+      <h2 class="left-wrapper" id="software">Software.</h2>
     </VerticalBox>
   </main>
 </template>
